8ea55421a7ce26c833ceb3867d1e52a825592b7d
gabtot <gabor.toth.de@digikabel.hu>
2021-11-15 01:06:14 +0100

    kész

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,35 @@
 
 
+const setCookie = (name,value) => {
+    const now = new Date();
+    now.setTime( now.getTime() + 15*60*1000);
+    document.cookie = `${name}=${value}; expires=${now.toUTCString()}`;
+}
+setCookie('viewed','5');
+setCookie('uid','354774631237');
+setCookie('ssid','Bx55OWbHJ0Vt_IGIF');
 
+const cookieHandler = {
+    getAll() {
+        return document.cookie.split('; ')
+            .reduce(
+                (prev,curr)=> (prev[ curr.split('=')[0] ] = curr.split('=')[1]) && prev,
+                {}
+            );
+    },
+    toSessionStorage() {
+        let allCookie = cookieHandler.getAll();
+        for (let k in allCookie) {
+            sessionStorage.setItem( k, allCookie[k] );
+        }
+    },
+    flush() {
+        let allCookie = cookieHandler.getAll();
+        for (let k in allCookie) {
+            document.cookie = `${k}=; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
+        }
+    }
 
+}
 
-
-// export { cookieHandler };
+export { cookieHandler };
